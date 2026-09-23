db4b95449d7942dbdb041360b2e842477977227d
yas0reo <yass.onekeo@gmail.com>
2026-09-23 17:59:24 -0400

    Refactor laboratoire_1.ino for button and LED configuration; add laboratoire_1.pd for audio processing

--- laboratoire_1.ino
+++ laboratoire_1.ino
@@ -1,12 +1,11 @@
 #include <Arduino.h>
-#include <Chrono.h>
 #include <Bounce2.h>
  
-#define BROCHE_DEL_TEMPO 8
-#define BROCHE_BOUTON_TEMPO 7
+#define BROCHE_DEL_TEMPO 5
+#define BROCHE_BOUTON_TEMPO 6
 
-#define BROCHE_DEL_BAS 6
-#define BROCHE_BOUTON_BAS 5
+#define BROCHE_DEL_BAS 7
+#define BROCHE_BOUTON_BAS 8
 
 Bounce2::Button BOUTON_TEMPO = Bounce2::Button();
 Bounce2::Button BOUTON_BAS = Bounce2::Button();
@@ -22,7 +21,7 @@ void setup()
 
     // Configuration de la DEL
     pinMode(BROCHE_DEL_BAS, OUTPUT);
-    digitalWrite(BROCHE_DEL_BAS, etatDelBas);
+    digitalWrite(BROCHE_DEL_BAS, etatDelTempo);
  
 
     // Configuration du bouton
@@ -42,33 +41,35 @@ void loop()
     if (BOUTON_TEMPO.isPressed())
     {
         digitalWrite(BROCHE_DEL_TEMPO, HIGH); //lumière est allumée
-        Serial.print("TEMPO"); // Descripteur
+        Serial.print("bouton6 "); // Descripteur
         Serial.print(" "); // Espace
         Serial.print(1); // Valeur
         Serial.println(); // Saut de ligne
+
+
     }
     else
     {
-        digitalWrite(BROCHE_DEL_TEMPO, etatDelTempo); //lumière est éteinte
+        digitalWrite(BROCHE_DEL_TEMPO, LOW); //lumière est éteinte
     }
  
-    if (BOUTON_BAS.isPressed())
+    if (BOUTON_BAS.pressed())
     {
-        digitalWrite(BROCHE_DEL_BAS, HIGH); //lumière est allumée
-        Serial.print("BAS"); // Descripteur
+        digitalWrite(BROCHE_DEL_BAS, HIGH);
+        Serial.print("bouton8 "); // Descripteur
         Serial.print(" "); // Espace
-        Serial.print(2); // Valeur
+        Serial.print(1); // Valeur
         Serial.println(); // Saut de ligne
+
         if ( etatDelBas == 0 ) {
             etatDelBas = 1;
         } else {
             etatDelBas = 0;    
         }
+
+       
     }
-    else
-    {
-        digitalWrite(BROCHE_DEL_BAS, etatDelBas); //lumière est éteinte
-    }
+    digitalWrite(BROCHE_DEL_BAS, etatDelBas);
     
 }
  
